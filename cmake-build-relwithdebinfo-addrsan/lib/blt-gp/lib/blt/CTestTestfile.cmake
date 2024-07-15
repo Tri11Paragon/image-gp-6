dc7e2d54dd07eb5cafdd2e3b8d29521ff3a03f9d
Brett <brettmaster1@gmail.com>
2024-07-15 00:03:55 -0400

    remove cmake

--- cmake-build-relwithdebinfo-addrsan/lib/blt-gp/lib/blt/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brett/Documents/code/c++/image-gp-6/lib/blt-gp/lib/blt
-# Build directory: /home/brett/Documents/code/c++/image-gp-6/cmake-build-relwithdebinfo-addrsan/lib/blt-gp/lib/blt
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-relwithdebinfo/lib/blt-gp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brett/Documents/code/c++/image-gp-6/lib/blt-gp
-# Build directory: /home/brett/Documents/code/c++/image-gp-6/cmake-build-relwithdebinfo/lib/blt-gp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib/blt")
